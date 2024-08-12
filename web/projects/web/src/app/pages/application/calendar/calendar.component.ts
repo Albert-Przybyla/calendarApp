d586bc51c9a7dc6ed5265fca46cc6b8d649e491e
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2024-08-12 18:02:45 +0200

    fix

--- web/projects/web/src/app/pages/application/calendar/calendar.component.ts
+++ web/projects/web/src/app/pages/application/calendar/calendar.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { baseAnimations } from '../../../base/baseAnimations';
+import { CalendarComponent as Calendar } from '../../../components/calendar/calendar.component';
 
 @Component({
-  selector: 'app-calendar',
+  selector: 'app-calendar-page',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
   standalone: true,
+  imports: [Calendar],
   animations: baseAnimations,
 })
 export class CalendarComponent implements OnInit {
